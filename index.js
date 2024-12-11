fa79e159ec17b799b8c0aa67ed6753742af3fa1f
arpanb8907 <58073354+arpanb8907@users.noreply.github.com>
2024-12-11 23:05:49 +0530

    backend server ip added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose';
 // import sampleRoutes from './routes/sampleRoutes.js'; 
-import router from "./routes/userroutes.js"
+import router from "./routes/userRoutes.js"
 import  dotenv from 'dotenv';
 
 // mongodb+srv://arpanbhowmick1312:a5kXatFBSoNsd4DF@cluster0.m97in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
@@ -13,7 +13,7 @@ const Corsoptions = {
     origin : 'http://localhost:3000',
 };
 
-const port = 5000
+const port = "https://school-management-backend-2k5j.onrender.com"
 const app = express();
 app.use(cors(Corsoptions))
 app.use(express.json());
